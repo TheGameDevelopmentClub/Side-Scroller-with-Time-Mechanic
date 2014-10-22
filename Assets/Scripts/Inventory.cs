827ebf44c3b7c015b11f3a5b1ae1b161e13d9b27
Ninjtastic <ryanwoodburn92@gmail.com>
2014-10-21 21:35:29 -0500

    Inventory Started
    Created some scripts for representing items, inventory, and an item
    database

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour {
+
+	public List<Item> inventory = new List<Item>();
+	public int selected = 0;
+	private ItemDatabase database;
+	public Texture2D selector;
+
+	// Use this for initialization
+	void Start () {
+		database = GameObject.FindGameObjectWithTag ("ItemDatabase").GetComponent<ItemDatabase>();
+		for (int i = 0; i < database.items.Count; i++) 
+		{
+			inventory.Add (database.items [i]);
+		}
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			if (selected == 0) selected = inventory.Count - 1;
+			else selected--;
+		}
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			if (selected == inventory.Count - 1) selected = 0;
+			else selected++;
+		}
+	}
+
+	void OnGUI()
+	{
+		for(int i = 0; i < inventory.Count; i++)
+		{
+			GUI.Label(new Rect(10, (i * 50) + 15, 200, 50), inventory[i].name);
+			GUI.Label(new Rect(70, i * 50, 200, 50), inventory[i].icon);
+			if (selected == i) GUI.Label(new Rect(0, i * 50, 500, 50), selector);
+		}
+	}
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Item {
+
+	public string name;
+	public Texture2D icon;
+	public string description;
+	public ItemType itemType;
+
+	public enum ItemType {
+		gun,
+		melee,
+		buff
+	}
+}
